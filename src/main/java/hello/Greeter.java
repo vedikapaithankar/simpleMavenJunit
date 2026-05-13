a04f96d298ace0b06d647e827acad985ce2429ab
Vedika Paithankar <vedikapaithankar05@gmail.com>
2026-05-13 11:56:44 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Welcome to Unnati Development Version 2....." !!!! Bye";
+    return "Welcome to Unnati Development Version 2 !!!! Bye";
   }
 }
